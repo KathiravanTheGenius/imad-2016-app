bbd5b549b78c89de1213599444acb8b11c8bc50d
KathiravanTheGenius <kathiravansingaravalen@gmail.com>
2016-09-19 18:37:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/SimCity Panoramic.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'Simcity', 'Panoramic.png'));
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 
